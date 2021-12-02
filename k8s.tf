abcdddfa0a71305b65d4dd456d09bebd92ead762
Ondrej Sika <ondrej@ondrejsika.com>
2021-12-02 17:40:23 +0100

    feat(k8s): Update Kubernetes version

--- k8s.tf
+++ k8s.tf
@@ -2,7 +2,7 @@ resource "digitalocean_kubernetes_cluster" "sikademo" {
   name   = "sikademo"
   region = "fra1"
   // Get available versions using: doctl kubernetes options versions
-  version = "1.20.8-do.0"
+  version = "1.21.5-do.0"
 
   node_pool {
     name = "sikademo"
