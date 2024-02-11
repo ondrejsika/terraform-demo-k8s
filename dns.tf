96af3fe10a736499dbe17df435108e5a91202e86
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-11 18:12:41 +0100

    feat: Add k8s-{prod,dev}.sikademo.com (with wildcart) pointing to LB

--- dns.tf
+++ dns.tf
@@ -13,3 +13,35 @@ resource "cloudflare_record" "k8s_wildcard" {
   type    = "CNAME"
   proxied = false
 }
+
+resource "cloudflare_record" "k8s_prod" {
+  zone_id = local.sikademo_com_zone_id
+  name    = "${cloudflare_record.k8s.name}-prod"
+  value   = cloudflare_record.k8s.hostname
+  type    = "CNAME"
+  proxied = false
+}
+
+resource "cloudflare_record" "k8s_prod_wildcard" {
+  zone_id = local.sikademo_com_zone_id
+  name    = "*.${cloudflare_record.k8s_prod.name}"
+  value   = cloudflare_record.k8s.hostname
+  type    = "CNAME"
+  proxied = false
+}
+
+resource "cloudflare_record" "k8s_dev" {
+  zone_id = local.sikademo_com_zone_id
+  name    = "${cloudflare_record.k8s.name}-dev"
+  value   = cloudflare_record.k8s.hostname
+  type    = "CNAME"
+  proxied = false
+}
+
+resource "cloudflare_record" "k8s_dev_wildcard" {
+  zone_id = local.sikademo_com_zone_id
+  name    = "*.${cloudflare_record.k8s_dev.name}"
+  value   = cloudflare_record.k8s.hostname
+  type    = "CNAME"
+  proxied = false
+}
