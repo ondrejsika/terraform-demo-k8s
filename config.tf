238550c08c05784640c5e4ead51db6b40748de81
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-22 09:12:14 +0200

    feat(k8s): Update Kubernetes version

--- config.tf
+++ config.tf
@@ -19,7 +19,7 @@ locals {
   // Get available regions using: doctl kubernetes options regions
   region = "fra1"
   // Get available versions using: doctl kubernetes options versions
-  k8s_version = "1.36.0-do.0"
+  k8s_version = "1.36.0-do.1"
   // Get available sizes using: doctl kubernetes options sizes
   node_size  = "s-4vcpu-8gb"
   node_count = 3
